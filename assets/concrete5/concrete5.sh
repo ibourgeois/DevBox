dfad4ea879ac8ceb9d2e7320a154448e4f216e68
Derek Bourgeois <derek@ibourgeois.com>
2014-03-30 01:56:12 -0400

    Create concrete5.sh

---
+++ assets/concrete5/concrete5.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+clear
+echo "Installing Concrete5"
+
+# Install Concrete5
+cd /var/www
+wget --trust-server-names http://www.concrete5.org/download_file/-/view/58379/8497/ -O concrete5.zip
+sudo unzip concrete5.zip
+sudo mv concrete5.6.2.1 concrete5
+
+# MODIFY 000-default 
+sudo : > /etc/apache2/sites-enabled/000-default
+echo "<VirtualHost *:80>" > /etc/apache2/sites-enabled/000-default
+echo "	ServerAdmin webmaster@localhost" >> /etc/apache2/sites-enabled/000-default
+echo "" >> /etc/apache2/sites-enabled/000-default
+echo "	DocumentRoot /var/www/concrete5" >> /etc/apache2/sites-enabled/000-default
+echo "	<Directory />" >> /etc/apache2/sites-enabled/000-default
+echo "		Options FollowSymLinks" >> /etc/apache2/sites-enabled/000-default
+echo "		AllowOverride All" >> /etc/apache2/sites-enabled/000-default
+echo "	</Directory>" >> /etc/apache2/sites-enabled/000-default
+echo "	<Directory /var/www/concrete5/>" >> /etc/apache2/sites-enabled/000-default
+echo "		Options Indexes FollowSymLinks MultiViews" >> /etc/apache2/sites-enabled/000-default
+echo "		AllowOverride None" >> /etc/apache2/sites-enabled/000-default
+echo "		Order allow,deny" >> /etc/apache2/sites-enabled/000-default
+echo "		allow from all" >> /etc/apache2/sites-enabled/000-default
+echo "	</Directory>" >> /etc/apache2/sites-enabled/000-default
+echo "" >> /etc/apache2/sites-enabled/000-default
+echo "	ScriptAlias /cgi-bin/ /usr/lib/cgi-bin/" >> /etc/apache2/sites-enabled/000-default
+echo "	<Directory \"/usr/lib/cgi-bin\">" >> /etc/apache2/sites-enabled/000-default
+echo "		AllowOverride None" >> /etc/apache2/sites-enabled/000-default
+echo "		Options +ExecCGI -MultiViews +SymLinksIfOwnerMatch" >> /etc/apache2/sites-enabled/000-default
+echo "		Order allow,deny" >> /etc/apache2/sites-enabled/000-default
+echo "		Allow from all" >> /etc/apache2/sites-enabled/000-default
+echo "	</Directory>" >> /etc/apache2/sites-enabled/000-default
+echo "" >> /etc/apache2/sites-enabled/000-default
+echo "	ErrorLog ${APACHE_LOG_DIR}/error.log" >> /etc/apache2/sites-enabled/000-default
+echo "" >> /etc/apache2/sites-enabled/000-default
+echo "	# Possible values include: debug, info, notice, warn, error, crit," >> /etc/apache2/sites-enabled/000-default
+echo "	# alert, emerg." >> /etc/apache2/sites-enabled/000-default
+echo "	LogLevel warn" >> /etc/apache2/sites-enabled/000-default
+echo "" >> /etc/apache2/sites-enabled/000-default
+echo "	CustomLog ${APACHE_LOG_DIR}/access.log combined" >> /etc/apache2/sites-enabled/000-default
+echo "" >> /etc/apache2/sites-enabled/000-default
+echo "    Alias /doc/ \"/usr/share/doc/\"" >> /etc/apache2/sites-enabled/000-default
+echo "    <Directory \"/usr/share/doc/\">" >> /etc/apache2/sites-enabled/000-default
+echo "        Options Indexes MultiViews FollowSymLinks" >> /etc/apache2/sites-enabled/000-default
+echo "        AllowOverride None" >> /etc/apache2/sites-enabled/000-default
+echo "        Order deny,allow" >> /etc/apache2/sites-enabled/000-default
+echo "        Deny from all" >> /etc/apache2/sites-enabled/000-default
+echo "        Allow from 127.0.0.0/255.0.0.0 ::1/128" >> /etc/apache2/sites-enabled/000-default
+echo "    </Directory>" >> /etc/apache2/sites-enabled/000-default
+echo "" >> /etc/apache2/sites-enabled/000-default
+echo "</VirtualHost>" >> /etc/apache2/sites-enabled/000-default
+
+# Restart Apache
+sudo service apache2 restart
+
+# Set up the database
+echo "CREATE DATABASE IF NOT EXISTS concrete5" | mysql
+echo "CREATE USER 'vagrant'@'localhost' IDENTIFIED BY 'vagrant'" | mysql
+echo "GRANT ALL PRIVILEGES ON concrete5.* TO 'vagrant'@'localhost' IDENTIFIED BY 'vagrant'" | mysql
