06cf9be099869ee24875fdf6eb9a5a0c400829d2
Derek Bourgeois <derek@ibourgeois.com>
2014-03-31 11:43:14 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -25,55 +25,55 @@ echo "Enter your selection (1-14): \c"
 read choice
 
 case "$choice" in
-	1)  cd assets/cakephp
+	1)  cd ${0%/*}/assets/cakephp
 		vagrant box add cakephp http://files.vagrantup.com/precise32.box --force
 		vagrant up
 		vagrant ssh ;;
-	2)  cd assets/codeigniter
+	2)  cd ${0%/*}/assets/codeigniter
 		vagrant box add codeigniter http://files.vagrantup.com/precise32.box --force
 		vagrant up
 		vagrant ssh ;;
-	3)  cd assets/concrete5
+	3)  cd ${0%/*}/assets/concrete5
 		vagrant box add concrete5 http://files.vagrantup.com/precise32.box --force
 		vagrant up
 		vagrant ssh ;;
-	4)  cd assets/drupal
+	4)  cd ${0%/*}/assets/drupal
 		vagrant box add drupal http://files.vagrantup.com/precise32.box --force
 		vagrant up
 		vagrant ssh ;;
-	5)  cd assets/joomla
+	5)  cd ${0%/*}/assets/joomla
 		vagrant box add joomla http://files.vagrantup.com/precise32.box --force
 		vagrant up
 		vagrant ssh ;;
-	6)  cd assets/laravel
+	6)  cd ${0%/*}/assets/laravel
 		vagrant box add laravel http://files.vagrantup.com/precise32.box --force
 		vagrant up
 		vagrant ssh ;;
-	7)  cd assets/phpbb
+	7)  cd ${0%/*}/assets/phpbb
 		vagrant box add phpbb http://files.vagrantup.com/precise32.box --force
 		vagrant up
 		vagrant ssh ;;
-	8)  cd assets/rubyonrails
+	8)  cd ${0%/*}/assets/rubyonrails
 		vagrant box add rubyonrails http://files.vagrantup.com/precise32.box --force
 		vagrant up
 		vagrant ssh ;;
-	9)  cd assets/silverstripe
+	9)  cd ${0%/*}/assets/silverstripe
 		vagrant box add silverstripe http://files.vagrantup.com/precise32.box --force
 		vagrant up
 		vagrant ssh ;;
-	10) cd assets/symfony
+	10) cd ${0%/*}/assets/symfony
 		vagrant box add symfony http://files.vagrantup.com/precise32.box --force
 		vagrant up
 		vagrant ssh ;;
-	11) cd assets/wordpress
+	11) cd ${0%/*}/assets/wordpress
 	    vagrant box add wordpress http://files.vagrantup.com/precise32.box --force
 	    vagrant up 
 	    vagrant ssh ;;
-	12) cd assets/yii
+	12) cd ${0%/*}/assets/yii
 		vagrant box add yii http://files.vagrantup.com/precise32.box --force
 		vagrant up
 		vagrant ssh ;;
-	13) cd assets/zend
+	13) cd ${0%/*}/assets/zend
 		vagrant box add zend http://files.vagrantup.com/precise32.box --force
 		vagrant up
 		vagrant ssh ;;
