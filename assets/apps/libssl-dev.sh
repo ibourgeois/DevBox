96efaef7404f709c63c35e768927b3547ef12a1a
Derek Bourgeois <derek@ibourgeois.com>
2014-03-31 08:32:36 -0400

    Update libssl-dev.sh

--- assets/apps/libssl-dev.sh
+++ assets/apps/libssl-dev.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 clear
+echo "================================================================================"
 echo "Installing LibSSL-Dev"
+echo "================================================================================"
 
 # Install LibSSL-Dev
 sudo apt-get install -y libssl-dev
