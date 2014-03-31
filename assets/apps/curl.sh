b9e88ff7b0c65360923cb4314d0d62b2b9c7c622
Derek Bourgeois <derek@ibourgeois.com>
2014-03-31 08:29:11 -0400

    Update curl.sh

--- assets/apps/curl.sh
+++ assets/apps/curl.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 clear
+echo "================================================================================"
 echo "Installing cURL"
+echo "================================================================================"
 
 # Install cURL
 sudo apt-get install -y php5-curl
 sudo apt-get install -y curl
+sudo apt-get install -y libcurl4-openssl-dev
