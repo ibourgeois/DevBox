95eccfebab771c44110958c67757775c877669a9
Derek Bourgeois <derek@ibourgeois.com>
2014-03-31 08:31:40 -0400

    Update nodejs.sh

--- assets/apps/nodejs.sh
+++ assets/apps/nodejs.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 clear
+echo "================================================================================"
 echo "Installing Node.JS"
+echo "================================================================================"
 
 # Install Node.JS
 sudo apt-get install -y nodejs
