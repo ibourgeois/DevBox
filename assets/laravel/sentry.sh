b25da5ae62a0a4309ca2a74f30a5ae6ee3072078
Derek Bourgeois <derek@ibourgeois.com>
2014-05-31 22:16:01 -0400

    Update sentry.sh

--- assets/laravel/sentry.sh
+++ assets/laravel/sentry.sh
@@ -1,1 +1,17 @@
-#bin
+#!/usr/bin/env bash
+clear
+echo "================================================================================"
+echo "Installing Sentry"
+echo "================================================================================"
+
+# Install Sentry
+cd /var/www/laravel
+
+
+# Add Sentry to the required packages in composer.json
+# "cartalyst/sentry": "2.1.*"
+
+# Run Composer Update to install Sentry
+# php composer update
+
+
