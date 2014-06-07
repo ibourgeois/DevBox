ba211fc9ee70d4001bf691651cf889e28762638c
Derek Bourgeois <derek@ibourgeois.com>
2014-06-07 03:03:20 -0400

    Update vsftpd.sh

--- assets/apps/vsftpd.sh
+++ assets/apps/vsftpd.sh
@@ -1,26 +1,15 @@
 #!/usr/bin/env bash
 
 clear
-echo "================================================================================"
+echo "============================================================"
 echo "Installing VSFTPD"
-echo "================================================================================"
+echo "============================================================"
 
 #Install VSFTPD
 sudo apt-get install -y vsftpd
 
 # Modify VSFTPD.CONF
-sudo : > /etc/vsftpd.conf
-echo "listen=YES" > /etc/vsftpd.conf
-echo "anonymous_enable=NO" >> /etc/vsftpd.conf
-echo "local_enable=YES" >> /etc/vsftpd.conf
-echo "write_enable=YES" >> /etc/vsftpd.conf
-echo "dirmessage_enable=YES" >> /etc/vsftpd.conf
-echo "use_localtime=YES" >> /etc/vsftpd.conf
-echo "xferlog_enable=YES" >> /etc/vsftpd.conf
-echo "connect_from_port_20=YES" >> /etc/vsftpd.conf
-echo "secure_chroot_dir=/var/run/vsftpd/empty" >> /etc/vsftpd.conf
-echo "pam_service_name=vsftpd" >> /etc/vsftpd.conf
-echo "rsa_cert_file=/etc/ssl/private/vsftpd.pem" >> /etc/vsftpd.conf
+sudo sed -i 's/#write_enable=YES/write_enable=YES/' /etc/vsftpd.conf
 
 # Restart VSFTPD
 sudo service vsftpd restart
