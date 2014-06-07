d4826c0ba096f4e006f8bda7bf4cbbfeb8fecdbc
Derek Bourgeois <derek@ibourgeois.com>
2014-06-07 02:52:30 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -26,56 +26,69 @@ read choice
 
 case "$choice" in
 	1)  cd ${0%/*}/assets/cakephp
-		vagrant box add cakephp http://files.vagrantup.com/precise32.box --force
+		vagrant box add devbox http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box --force
 		vagrant up
+		bash welcome.sh
 		vagrant ssh ;;
 	2)  cd ${0%/*}/assets/codeigniter
-		vagrant box add codeigniter http://files.vagrantup.com/precise32.box --force
+		vagrant box add devbox http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box --force
 		vagrant up
+		bash welcome.sh
 		vagrant ssh ;;
 	3)  cd ${0%/*}/assets/concrete5
-		vagrant box add concrete5 http://files.vagrantup.com/precise32.box --force
+		vagrant box add devbox http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box --force
 		vagrant up
+		bash welcome.sh
 		vagrant ssh ;;
 	4)  cd ${0%/*}/assets/drupal
-		vagrant box add drupal http://files.vagrantup.com/precise32.box --force
+		vagrant box add devbox http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box --force
 		vagrant up
+		bash welcome.sh
 		vagrant ssh ;;
 	5)  cd ${0%/*}/assets/joomla
-		vagrant box add joomla http://files.vagrantup.com/precise32.box --force
+		vagrant box add devbox http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box --force
 		vagrant up
+		bash welcome.sh
 		vagrant ssh ;;
 	6)  cd ${0%/*}/assets/laravel
-		vagrant box add laravel http://files.vagrantup.com/precise32.box --force
+		vagrant box add devbox http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box --force
 		vagrant up
+		bash welcome.sh
 		vagrant ssh ;;
 	7)  cd ${0%/*}/assets/phpbb
-		vagrant box add phpbb http://files.vagrantup.com/precise32.box --force
+		vagrant box add devbox http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box --force
 		vagrant up
+		bash welcome.sh
 		vagrant ssh ;;
 	8)  cd ${0%/*}/assets/rubyonrails
-		vagrant box add rubyonrails http://files.vagrantup.com/precise32.box --force
+		vagrant box add devbox http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box --force
 		vagrant up
+		bash welcome.sh
 		vagrant ssh ;;
 	9)  cd ${0%/*}/assets/silverstripe
-		vagrant box add silverstripe http://files.vagrantup.com/precise32.box --force
+		vagrant box add devbox http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box --force
 		vagrant up
+		bash welcome.sh
 		vagrant ssh ;;
 	10) cd ${0%/*}/assets/symfony
-		vagrant box add symfony http://files.vagrantup.com/precise32.box --force
+		vagrant box add devbox http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box --force
 		vagrant up
+		bash welcome.sh
 		vagrant ssh ;;
 	11) cd ${0%/*}/assets/wordpress
-	    vagrant box add wordpress http://files.vagrantup.com/precise32.box --force
+	    vagrant box add devbox http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box --force
 	    vagrant up 
+	    bash welcome.sh
 	    vagrant ssh ;;
 	12) cd ${0%/*}/assets/yii
-		vagrant box add yii http://files.vagrantup.com/precise32.box --force
+		vagrant box add devbox http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box --force
 		vagrant up
+		bash welcome.sh
 		vagrant ssh ;;
 	13) cd ${0%/*}/assets/zend
-		vagrant box add zend http://files.vagrantup.com/precise32.box --force
+		vagrant box add devbox http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box --force
 		vagrant up
+		bash welcome.sh
 		vagrant ssh ;;
 	14) exit ;;
 esac
