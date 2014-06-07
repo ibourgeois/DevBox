e4ee08c7280e585b1220ae3eaac75cba8b25e82c
Derek Bourgeois <derek@ibourgeois.com>
2014-06-07 02:58:05 -0400

    Update completed.sh

--- assets/apps/completed.sh
+++ assets/apps/completed.sh
@@ -1,22 +1,37 @@
 #!/usr/bin/env bash
+
 clear
+
 ip=$(ifconfig eth1 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
-echo "================================================================================"
+
+touch /vagrant/welcome.sh
+echo "#!/usr/bin/env bash" > /vagrant/welcome.sh
+echo "" >> /vagrant/welcome.sh
+echo "xdg-open 'http://$ip'" >> /vagrant/welcome.sh
+echo "gnome-open 'http://$ip'" >> /vagrant/welcome.sh
+echo "open 'http://$ip'" >> /vagrant/welcome.sh
+
+touch /vagrant/welcome.bat
+echo "@echo off" > /vagrant/welcome.bat
+echo "start http://$ip" >> /vagrant/welcome.bat
+
+
+echo "============================================================"
 echo " Installation Has Completed"
-echo "================================================================================"
+echo "============================================================"
 echo " "
-echo "  You can access your site by typing in the following address into your web"
-echo "  browser:"
+echo "  You can access your site by typing in the following "
+echo "  address into your web browser:"
 echo " "
-echo "                          http://$ip"
+echo "                  http://$ip"
 echo " "
 echo " "
 echo "  If you have any questions/issues, please visit:"
-echo "  http://github.com/ibourgeois/Vagrant"
+echo "  https://github.com/TechFix/TechFix-DevBox"
 echo " "
 echo "  Thanks,"
 echo "      Derek Bourgeois"
 echo "      derek@ibourgeois.com"
 echo "      http://ibourgeois.com"
 echo " "
-echo "================================================================================"
+echo "============================================================"
