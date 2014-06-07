8921f0aa53cf5e34a778cb1ca24a1796763c8a71
Derek Bourgeois <derek@ibourgeois.com>
2014-06-07 02:55:44 -0400

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -7,7 +7,7 @@ COLOR 4F
 CLS
 ECHO.
 ECHO             ******************************************************
-ECHO             *              DevBox v1.0 - Main Menu               *
+ECHO             *                 DevBox - Main Menu                 *
 ECHO             ******************************************************
 ECHO             *                                                    *
 ECHO             *   [1]  CakePHP                                     *
@@ -50,104 +50,117 @@ PAUSE
 :CAKEPHP
 CLS
 cd %CD%\assets\cakephp
-vagrant box add cakephp http://files.vagrantup.com/precise32.box --force
+vagrant box add devbox http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box --force
 vagrant up
+call welcome.bat
 vagrant ssh
 GOTO :EOF
 
 :CODEIGNITER
 CLS
 cd %CD%\assets\codeigniter
-vagrant box add codeigniter http://files.vagrantup.com/precise32.box --force
+vagrant box add devbox http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box --force
 vagrant up
+call welcome.bat
 vagrant ssh
 GOTO :EOF
 
 :CONCRETE5
 CLS
 cd %CD%\assets\concrete5
-vagrant box add concrete5 http://files.vagrantup.com/precise32.box --force
+vagrant box add devbox http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box --force
 vagrant up
+call welcome.bat
 vagrant ssh
 GOTO :EOF
 
 :DRUPAL
 CLS
 cd %CD%\assets\drupal
-vagrant box add drupal http://files.vagrantup.com/precise32.box --force
+vagrant box add devbox http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box --force
 vagrant up
+call welcome.bat
 vagrant ssh
 GOTO :EOF
 
 :JOOMLA
 CLS
 cd %CD%\assets\joomla
-vagrant box add joomla http://files.vagrantup.com/precise32.box --force
+vagrant box add devbox http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box --force
 vagrant up
+call welcome.bat
 vagrant ssh
 GOTO :EOF
 
 :LARAVEL
 CLS
 cd %CD%\assets\laravel
-vagrant box add laravel http://files.vagrantup.com/precise32.box --force
+vagrant box add devbox http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box --force
 vagrant up
+call welcome.bat
 vagrant ssh
 GOTO :EOF
 
 :PHPBB
 CLS
 cd %CD%\assets\phpbb
-vagrant box add phpbb http://files.vagrantup.com/precise32.box --force
+vagrant box add devbox http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box --force
 vagrant up
+call welcome.bat
 vagrant ssh
 GOTO :EOF
 
 :RUBYONRAILS
 CLS
 cd %CD%\assets\rubyonrails
-vagrant box add rubyonrails http://files.vagrantup.com/precise32.box --force
+vagrant box add devbox http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box --force
 vagrant up
+call welcome.bat
 vagrant ssh
 GOTO :EOF
 
 :SILVERSTRIPE
 CLS
 cd %CD%\assets\silverstripe
-vagrant box add silverstripe http://files.vagrantup.com/precise32.box --force
+vagrant box add devbox http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box --force
 vagrant up
+call welcome.bat
 vagrant ssh
 GOTO :EOF
 
 :SYMFONY
 CLS
 cd %CD%\assets\symfony
-vagrant box add symfony http://files.vagrantup.com/precise32.box --force
+vagrant box add devbox http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box --force
 vagrant up
+call welcome.bat
 vagrant ssh
 GOTO :EOF
 
 :WORDPRESS
 CLS
 cd %CD%\assets\wordpress
-vagrant box add wordpress http://files.vagrantup.com/precise32.box --force
+vagrant box add devbox http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box --force
 vagrant up
+call welcome.bat
 vagrant ssh
 GOTO :EOF
 
 :YII
 CLS
 cd %CD%\assets\yii
-vagrant box add yii http://files.vagrantup.com/precise32.box --force
+vagrant box add devbox http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box --force
 vagrant up
+call welcome.bat
 vagrant ssh
 GOTO :EOF
 
 :ZEND
 CLS
 cd %CD%\assets\zend
-vagrant box add zend http://files.vagrantup.com/precise32.box --force
+vagrant box add devbox http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box --force
 vagrant up
+call welcome.bat
 vagrant ssh
 GOTO :EOF
 
