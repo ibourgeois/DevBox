34a58b95b82945a1440d22da61510c70f9081865
Derek Bourgeois <derek@ibourgeois.com>
2014-06-08 14:27:48 -0400

    Update and rename phpmyadmin to phpmyadmin.sh

---
+++ assets/apps/phpmyadmin.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+clear
+echo "============================================================"
+echo "Installing phpMyAdmin"
+echo "============================================================"
+
+# Install phpMyAdmin
+sudo apt-get install -y phpmyadmin
