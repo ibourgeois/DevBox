4b6967d5950a9b4f912053de053cd33e002ee368
Derek Bourgeois <derek@ibourgeois.com>
2014-06-07 22:16:08 -0400

    Update mysql.sh

--- assets/apps/mysql.sh
+++ assets/apps/mysql.sh
@@ -10,5 +10,13 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get -q -y install mysql-server-5.5
 sudo apt-get install -y php5-mysql
 
+# Allow Remote Connections
+sudo sed -i '47 s|127.0.0.1|0.0.0.0|' /etc/mysql/my.cnf
+sudo service mysql restart
+mysql -u root -e "CREATE USER 'vagrant'@'localhost' IDENTIFIED BY 'vagrant'"
+mysql -u root -e "CREATE USER 'vagrant'@'%' IDENTIFIED BY 'vagrant'"
+mysql -u root -e "GRANT ALL ON *.* TO 'vagrant'@'localhost'"
+mysql -u root -e "GRANT ALL ON *.* TO 'vagrant'@'%'"
+
 # Restart Apache
 sudo service apache2 restart
