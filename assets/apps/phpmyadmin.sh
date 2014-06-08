b6512afcc593dcf62f87aa390d44bc0c6b03bc21
Derek Bourgeois <derek@ibourgeois.com>
2014-06-08 14:37:04 -0400

    Update phpmyadmin.sh

--- assets/apps/phpmyadmin.sh
+++ assets/apps/phpmyadmin.sh
@@ -6,4 +6,10 @@ echo "Installing phpMyAdmin"
 echo "============================================================"
 
 # Install phpMyAdmin
-sudo apt-get install -y phpmyadmin
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get install -q -y phpmyadmin
+echo 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/dbconfig-install boolean true' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/app-password-confirm password vagrant' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/mysql/admin-pass password vagrant' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/mysql/app-pass password vagrant' | debconf-set-selections
